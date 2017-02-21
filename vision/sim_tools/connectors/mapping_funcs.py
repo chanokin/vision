2114013062e8f10c1060c98d05f020160760eb29
Garibaldi Pineda Garcia <chanokin@gmail.com>
2017-02-21 15:51:41 +0000

    convert row_bits to uint32

--- vision/sim_tools/connectors/mapping_funcs.py
+++ vision/sim_tools/connectors/mapping_funcs.py
@@ -1,7 +1,8 @@
 import numpy as np
 
 def row_col_to_input(row, col, is_on_input, row_bits):
-    # row_bits = np.uint8(np.ceil(np.log2(height)))
+
+    row_bits = np.uint32(row_bits)
     idx = np.uint32(0)
     
     if is_on_input:
@@ -10,14 +11,11 @@ def row_col_to_input(row, col, is_on_input, row_bits):
     idx = idx | (row << 1)
     idx = idx | (col << (row_bits + 1))
     
-    #add two to allow for special event bits
-    # idx += 2
-    
     return idx
 
 
 def row_col_to_input_breakout(row, col, is_on_input, row_bits):
-    # row_bits = np.uint32(8)
+    row_bits = np.uint32(row_bits)
     idx = np.uint32(0)
     
     if is_on_input:
